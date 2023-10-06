7e46e6796f00c84df3bdad6baca7eb9ab72f4a07
theaholtlund <thea.holtlund.jacobsen@politiet.no>
2023-10-06 10:13:34 +0200

    Update SQL templates to add more testing data

--- sql-scripts/config.sql
+++ sql-scripts/config.sql
@@ -1,3 +1,64 @@
-CREATE DATABASE TestingDB;
-USE TestingDB;
-CREATE TABLE Inventory (id INT, name NVARCHAR(50), quantity INT);
+-- Basic configurations for container test databases
+
+-- Create and add tables to testing inventory database
+CREATE DATABASE InventoryDB;
+USE InventoryDB;
+
+CREATE TABLE Inventory (
+    id INT PRIMARY KEY,
+    name NVARCHAR(50),
+    quantity INT
+);
+
+CREATE TABLE Suppliers (
+    supplier_id INT PRIMARY KEY,
+    supplier_name NVARCHAR(100),
+    contact_name NVARCHAR(50),
+    contact_email NVARCHAR(100)
+);
+
+-- Create and add tables to testing customer database
+CREATE DATABASE CustomerDB;
+USE CustomerDB;
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    first_name NVARCHAR(50),
+    last_name NVARCHAR(50),
+    email NVARCHAR(100) UNIQUE
+);
+
+-- Create and add tables to testing orders database
+CREATE DATABASE OrdersDB;
+USE OrdersDB;
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE
+);
+
+CREATE TABLE OrderDetails (
+    detail_id INT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    price DECIMAL(10, 2),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
+
+CREATE TABLE Products (
+    product_id INT PRIMARY KEY,
+    product_name NVARCHAR(100),
+    price DECIMAL(10, 2)
+);
+
+CREATE TABLE ProductReviews (
+    review_id INT PRIMARY KEY,
+    product_id INT,
+    customer_id INT,
+    review_text NVARCHAR(MAX),
+    rating INT CHECK (rating >= 1 AND rating <= 5),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
--- sql-scripts/data-population.sql
+++ sql-scripts/data-population.sql
@@ -1,12 +1,45 @@
-INSERT INTO Inventory VALUES (1, 'banana', 150);
-INSERT INTO Inventory VALUES (2, 'orange', 154);
-INSERT INTO Inventory VALUES (3, 'apple', 10);
-INSERT INTO Inventory VALUES (4, 'bike', 17);
-INSERT INTO Inventory VALUES (5, 'chair', 12);
-INSERT INTO Inventory VALUES (6, 'table', 4);
-INSERT INTO Inventory VALUES (7, 'desk', 120);
-INSERT INTO Inventory VALUES (8, 'book', 142);
-INSERT INTO Inventory VALUES (9, 'glass', 100);
-INSERT INTO Inventory VALUES (10, 'jacket', 120);
-INSERT INTO Inventory VALUES (11, 'bread', 8);
-INSERT INTO Inventory VALUES (12, 'butter', 12);
+-- Template to populate container test databases with values
+
+-- Inventory data in InventoryDB
+USE InventoryDB;
+INSERT INTO Inventory VALUES (1, 'Banana', 150);
+INSERT INTO Inventory VALUES (2, 'Orange', 154);
+INSERT INTO Inventory VALUES (3, 'Apple', 10);
+INSERT INTO Inventory VALUES (4, 'Bike', 17);
+INSERT INTO Inventory VALUES (5, 'Chair', 12);
+INSERT INTO Inventory VALUES (6, 'Table', 4);
+INSERT INTO Inventory VALUES (7, 'Desk', 120);
+INSERT INTO Inventory VALUES (8, 'Book', 142);
+INSERT INTO Inventory VALUES (9, 'Glass', 100);
+INSERT INTO Inventory VALUES (10, 'Jacket', 120);
+INSERT INTO Inventory VALUES (11, 'Bread', 8);
+INSERT INTO Inventory VALUES (12, 'Butter', 12);
+
+-- Suppliers data in InventoryDB
+INSERT INTO Suppliers VALUES (1, 'Supplier A', 'John Doe', 'john@example.com');
+INSERT INTO Suppliers VALUES (2, 'Supplier B', 'Jane Smith', 'jane@example.com');
+INSERT INTO Suppliers VALUES (3, 'Supplier C', 'Bob Johnson', 'bob@example.com');
+
+-- Customers data in CustomerDB
+USE CustomerDB;
+INSERT INTO Customers VALUES (1, 'Alice', 'Smith', 'alice@example.com');
+INSERT INTO Customers VALUES (2, 'Bob', 'Johnson', 'bob@example.com');
+INSERT INTO Customers VALUES (3, 'Charlie', 'Brown', 'charlie@example.com');
+
+-- Orders and OrderDetails data in OrdersDB
+USE OrdersDB;
+INSERT INTO Orders VALUES (1, 1, '2023-10-01');
+INSERT INTO OrderDetails VALUES (1, 1, 1, 5, 10.99);
+INSERT INTO OrderDetails VALUES (2, 1, 2, 2, 5.49);
+INSERT INTO Orders VALUES (2, 2, '2023-10-02');
+INSERT INTO OrderDetails VALUES (3, 2, 3, 3, 7.99);
+
+-- Products data in OrdersDB
+INSERT INTO Products VALUES (1, 'Product X', 19.99);
+INSERT INTO Products VALUES (2, 'Product Y', 29.99);
+INSERT INTO Products VALUES (3, 'Product Z', 39.99);
+
+-- ProductReviews data in OrdersDB
+INSERT INTO ProductReviews VALUES (1, 1, 1, 'Great product!', 5);
+INSERT INTO ProductReviews VALUES (2, 1, 2, 'Good value for money', 4);
+INSERT INTO ProductReviews VALUES (3, 2, 1, 'Could be better', 3);
