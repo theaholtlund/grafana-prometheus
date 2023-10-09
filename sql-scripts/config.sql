e5f0e5642d9f46341853e29624e540152521b101
theaholtlund <thea.holtlund.jacobsen@politiet.no>
2023-10-09 12:24:07 +0200

    Update SQL scripts for execution in batches

--- sql-scripts/config.sql
+++ sql-scripts/config.sql
@@ -1,64 +1,114 @@
 -- Basic configurations for container test databases
 
--- Create and add tables to testing inventory database
-CREATE DATABASE InventoryDB;
+-- Use BEGIN and END blocks to group multiple SQL statements into a single transaction
+-- The GO statement is used to separate batches in scripts, where a batch is a set of statements sent to the server for execution as a group
+-- Batches are separated by a batch delimiter, as each batch is executed as a unit of work and SQL Server processes one batch at a time
+
+-- Create and add tables for Inventory testing database
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'InventoryDB')
+BEGIN
+    CREATE DATABASE InventoryDB;
+END
+GO
+
 USE InventoryDB;
+GO
 
-CREATE TABLE Inventory (
-    id INT PRIMARY KEY,
-    name NVARCHAR(50),
-    quantity INT
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Inventory')
+BEGIN
+    CREATE TABLE Inventory (
+        id INT PRIMARY KEY,
+        name NVARCHAR(50),
+        quantity INT
+    );
+END
+GO
 
-CREATE TABLE Suppliers (
-    supplier_id INT PRIMARY KEY,
-    supplier_name NVARCHAR(100),
-    contact_name NVARCHAR(50),
-    contact_email NVARCHAR(100)
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Suppliers')
+BEGIN
+    CREATE TABLE Suppliers (
+        supplier_id INT PRIMARY KEY,
+        supplier_name NVARCHAR(100),
+        contact_name NVARCHAR(50),
+        contact_email NVARCHAR(100)
+    );
+END
+GO
 
--- Create and add tables to testing customer database
+-- Create and add tables to Customer testing database
 CREATE DATABASE CustomerDB;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'CustomerDB')
+BEGIN
+    CREATE DATABASE CustomerDB;
+END
+GO
+
 USE CustomerDB;
+GO
 
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
-    first_name NVARCHAR(50),
-    last_name NVARCHAR(50),
-    email NVARCHAR(100) UNIQUE
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Customers')
+BEGIN
+    CREATE TABLE Customers (
+        customer_id INT PRIMARY KEY,
+        first_name NVARCHAR(50),
+        last_name NVARCHAR(50),
+        email NVARCHAR(100) UNIQUE
+    );
+END
+GO
 
--- Create and add tables to testing orders database
-CREATE DATABASE OrdersDB;
+-- Create and add tables to Orders testing database
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'OrdersDB')
+BEGIN
+    CREATE DATABASE OrdersDB;
+END
+GO
+
 USE OrdersDB;
+GO
 
-CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Orders')
+BEGIN
+    CREATE TABLE Orders (
+        order_id INT PRIMARY KEY,
+        customer_id INT,
+        order_date DATE
+    );
+END
+GO
 
-CREATE TABLE OrderDetails (
-    detail_id INT PRIMARY KEY,
-    order_id INT,
-    product_id INT,
-    quantity INT,
-    price DECIMAL(10, 2),
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'OrderDetails')
+BEGIN
+    CREATE TABLE OrderDetails (
+        detail_id INT PRIMARY KEY,
+        order_id INT,
+        product_id INT,
+        quantity INT,
+        price DECIMAL(10, 2),
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    );
+END
+GO
 
-CREATE TABLE Products (
-    product_id INT PRIMARY KEY,
-    product_name NVARCHAR(100),
-    price DECIMAL(10, 2)
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Products')
+BEGIN
+    CREATE TABLE Products (
+        product_id INT PRIMARY KEY,
+        product_name NVARCHAR(100),
+        price DECIMAL(10, 2)
+    );
+END
+GO
 
-CREATE TABLE ProductReviews (
-    review_id INT PRIMARY KEY,
-    product_id INT,
-    customer_id INT,
-    review_text NVARCHAR(MAX),
-    rating INT CHECK (rating >= 1 AND rating <= 5),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id),
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'ProductReviews')
+BEGIN
+    CREATE TABLE ProductReviews (
+        review_id INT PRIMARY KEY,
+        product_id INT,
+        order_id INT,
+        review_text NVARCHAR(MAX),
+        rating INT CHECK (rating >= 1 AND rating <= 5),
+        FOREIGN KEY (product_id) REFERENCES Products(product_id),
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    );
+END
--- sql-scripts/data-population.sql
+++ sql-scripts/data-population.sql
@@ -1,6 +1,6 @@
 -- Template to populate container test databases with values
 
--- Inventory data in InventoryDB
+-- Add data to Inventory table in Inventory testing database
 USE InventoryDB;
 INSERT INTO Inventory VALUES (1, 'Banana', 150);
 INSERT INTO Inventory VALUES (2, 'Orange', 154);
@@ -15,18 +15,18 @@ INSERT INTO Inventory VALUES (10, 'Jacket', 120);
 INSERT INTO Inventory VALUES (11, 'Bread', 8);
 INSERT INTO Inventory VALUES (12, 'Butter', 12);
 
--- Suppliers data in InventoryDB
+-- Add data to Suppliers table in Inventory testing database
 INSERT INTO Suppliers VALUES (1, 'Supplier A', 'John Doe', 'john@example.com');
 INSERT INTO Suppliers VALUES (2, 'Supplier B', 'Jane Smith', 'jane@example.com');
 INSERT INTO Suppliers VALUES (3, 'Supplier C', 'Bob Johnson', 'bob@example.com');
 
--- Customers data in CustomerDB
+-- Add data to Customers table in Customer testing database
 USE CustomerDB;
 INSERT INTO Customers VALUES (1, 'Alice', 'Smith', 'alice@example.com');
 INSERT INTO Customers VALUES (2, 'Bob', 'Johnson', 'bob@example.com');
 INSERT INTO Customers VALUES (3, 'Charlie', 'Brown', 'charlie@example.com');
 
--- Orders and OrderDetails data in OrdersDB
+-- Add data to Orders table in Orders testing database
 USE OrdersDB;
 INSERT INTO Orders VALUES (1, 1, '2023-10-01');
 INSERT INTO OrderDetails VALUES (1, 1, 1, 5, 10.99);
@@ -34,12 +34,12 @@ INSERT INTO OrderDetails VALUES (2, 1, 2, 2, 5.49);
 INSERT INTO Orders VALUES (2, 2, '2023-10-02');
 INSERT INTO OrderDetails VALUES (3, 2, 3, 3, 7.99);
 
--- Products data in OrdersDB
+-- Add data to Products table in Orders testing database
 INSERT INTO Products VALUES (1, 'Product X', 19.99);
 INSERT INTO Products VALUES (2, 'Product Y', 29.99);
 INSERT INTO Products VALUES (3, 'Product Z', 39.99);
 
--- ProductReviews data in OrdersDB
+-- Add data to ProductReviews table in Orders testing database
 INSERT INTO ProductReviews VALUES (1, 1, 1, 'Great product!', 5);
 INSERT INTO ProductReviews VALUES (2, 1, 2, 'Good value for money', 4);
 INSERT INTO ProductReviews VALUES (3, 2, 1, 'Could be better', 3);
