be772d747171e58943ba3a1812ed44d893b286fe
theaholtlund <thea.holtlund.jacobsen@politiet.no>
2023-11-15 13:10:17 +0100

    Add hypothetical logging for script execution

--- update-metrics.sh
+++ update-metrics.sh
@@ -4,5 +4,6 @@
 while true; do
   echo 'my_batch_job_completion_time' $(date +%s) > ./textfile-metrics/my_batch_job.prom.$$
   mv ./textfile-metrics/my_batch_job.prom.$$ ./textfile-metrics/my_batch_job.prom
+  echo 'Script executed at: $(date)' >> ./update-metrics-log.txt
   sleep 15
 done
