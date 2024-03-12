a21ebb0e8910adb4057763f4ba3c85a069c6f035
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-03-12 11:33:13 +0100

    Update file åath based on new directory structure

--- custom-collectors/update-metrics.sh
+++ custom-collectors/update-metrics.sh
@@ -3,8 +3,8 @@
 #!/bin/sh
 
 while true; do
-  echo 'my_batch_job_completion_time' $(date +%s) > ./textfile-metrics/my_batch_job.prom.$$
-  mv ./textfile-metrics/my_batch_job.prom.$$ ./textfile-metrics/my_batch_job.prom
-  echo 'Script executed at: $(date)' >> ./update-metrics-log.txt
+  echo 'my_batch_job_completion_time' $(date +%s) > ../textfile-metrics/my_batch_job.prom.$$
+  mv ../textfile-metrics/my_batch_job.prom.$$ ../textfile-metrics/my_batch_job.prom
+  echo 'Script executed at: $(date)' >> ../update-metrics-log.txt
   sleep 15
 done
